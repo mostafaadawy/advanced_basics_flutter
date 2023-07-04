55370adcbfe8ba209212828eac1cafafc7616db3
Mostafa Sayed Adawy <mostafa_adawy@ymail.com>
2023-07-04 17:30:16 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:advanced_basics_flutter/start_screen.dart';
+void main() {
+  runApp(MaterialApp(home: Scaffold(body: StartSceen()),));
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class StartSceen extends StatelessWidget {
+  const StartSceen({super.key});
+  @override
+  Widget build(context) {
+    return const Text(
+      'Start Screen',
+    );
+  }
+}
