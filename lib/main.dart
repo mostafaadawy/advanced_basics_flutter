effe7044bb0048080fd2346a8ddfa0480fe86d87
Mostafa Sayed Adawy <mostafa_adawy@ymail.com>
2023-07-04 17:31:41 +0300

    withouit comma , alt shift f formate does not work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:advanced_basics_flutter/start_screen.dart';
+
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: StartSceen()),));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartSceen(), // withouit comma , alt shift f formate does not work
+      ),
+    ),
+  );
 }
