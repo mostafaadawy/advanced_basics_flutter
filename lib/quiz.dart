92a7f1fefaaf40861d818a4535fca0c405df2c6b
Mostafa Sayed Adawy <mostafa_adawy@ymail.com>
2023-07-05 17:54:22 +0300

    end of questions condition

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:advanced_basics_flutter/start_screen.dart';
 import 'package:advanced_basics_flutter/questions_screen.dart';
-
+import 'package:advanced_basics_flutter/data/questions.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -13,7 +13,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  final List<String> selectedAnswers = [];
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
@@ -24,6 +24,13 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'start-screen';
+      });
+    }
   }
 
   @override
