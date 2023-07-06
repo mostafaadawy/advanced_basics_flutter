f0b3e411cb39ffcb7c33f293c3011ee38062bb8b
Mostafa Sayed Adawy <mostafa_adawy@ymail.com>
2023-07-06 12:16:40 +0300

    debugging

--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,6 @@ void main() {
     const Quiz(),
   );
 }
+// debuginging and break points and add to watch can be used to solve errors
+// see it from View-> appearance ->panel to see debug console if not exist select error  check above it to see the details and line of error then solve it
+// you can select debug instaede of run and add break points at the place you doubt or line before it and check the error value
